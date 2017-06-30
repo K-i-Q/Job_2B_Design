64e4502caef7a2f61676933168a30199ed86d145
eug3niio <geniohiphop@gmail.com>
2017-06-30 15:33:11 -0300

    Add files via upload
    Trabalho 3,5 - SITE PERFUME COMPLETO

---
+++ trabalho/js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+      $('.parallax').parallax();
+	}
+);
+$(document).ready(function(){
+      $('.slider').slider();
+    }
+);
+$('#enviar').on('click', function(){
+	alert('Dados Enviados com Sucesso. Entraremos em contato.');
+});
